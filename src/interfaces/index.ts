f014fbb627af8122208b56281232d69d598b9be0
Eric Lifs <lifszyc.eric@gmail.com>
2020-11-13 15:03:42 -0300

    Add preferredCitiesResponse interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,3 +3,4 @@ export type { default as CityInfo } from './cityInfo';
 export type { default as QueryParams } from './queryParams';
 export { default as FetchStatus } from './fetchStatus';
 export type { default as Favorites } from './favorites';
+export type { default as PreferredCitiesResponse } from './preferredCitiesResponse';
